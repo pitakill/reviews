147aa16c32d38edc65c4c51f7713c3efca8b3a74
Lepoldo Caballero <git@pitakill.net>
2018-04-09 21:59:30 -0500

    Adding React Router & Basic Routing

--- src/App/index.js
+++ src/App/index.js
@@ -3,6 +3,11 @@ import * as React from 'react';
 import theme from '@/toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import Grid from 'Components/Grid';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 import AppBar from 'react-toolbox/lib/app_bar/AppBar';
 import '@/toolbox/theme.css';
 
@@ -10,7 +15,12 @@ const App = ():React.Element<typeof Grid> => (
   <ThemeProvider {...{theme}}>
     <React.Fragment>
       <AppBar title="Music reviews"/>
-      <Grid />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Grid}/>
+          <Route exact path="/:id" component={() => <div>Hola</div>}/>
+        </Switch>
+      </BrowserRouter>
     </React.Fragment>
   </ThemeProvider>
 );
